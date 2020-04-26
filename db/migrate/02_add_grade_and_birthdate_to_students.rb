d4837b2efc0657ebde229caf89d9975b9e909e1c
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-26 03:28:05 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migration 
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
   add_column 
   :students, :grade, :integer 
   :students, :birthdate, :string 
