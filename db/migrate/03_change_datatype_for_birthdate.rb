fbddc192dc9cde3c64f9672a4fd9f0cd91a9ff39
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-26 03:32:12 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change 
-    change_column(:studnents, :birthdate, :datetime)
+    change_column:studnents, :birthdate, :datetime
   end 
 end 
